dd6858786a92472a4914fdd2bb86d2d110240f92
Student <student@donation.local>
2019-10-15 16:18:00 -0500

    Trying to still elaborate  on what goes where.

---
+++ templates/base.js
@@ -0,0 +1,7 @@
+const make_button = document.querySelector("#make_button")
+const get_entries = document.querySelectorAll(".get_entries")
+
+function updateEntries(data) {
+    const options = [];
+    for (const name of 
+}
